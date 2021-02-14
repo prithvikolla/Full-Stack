4032283aed6b8b439692db6fd1c57bd49a2ec187
prithvi <prithvikolla8@gmail.com>
2021-02-13 21:21:49 -0600

    my first react app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tweet from './Tweet';
+
+function App(){
+  return (
+<div>
+ <Tweet/>
+</div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+function Tweet(){
+    return(
+        <div className = 'tweet'>
+            <h2>Name</h2>
+            <p>This is my Random Tweet.</p>
+            <h4>Number of Likes</h4>
+        </div>
+    );
+}
+
+export default Tweet;
