40b8a6a9423987b55e2ee07b86217e378bf3ca39
prithvi <prithvikolla8@gmail.com>
2021-02-23 14:41:25 -0600

    update

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PinterestIcon from '@material-ui/icons/Pinterest';
+import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import styled from 'styled-components';
 function Header() {
@@ -91,5 +92,37 @@ const FollowingButton = styled(HomeButtons)`
     }
 `
 const SearchWrapper= styled.div`
-    
+    flex: 1; 
+`
+const SearchBarWrapper = styled.div`
+    display: flex;
+    background-color: #efefef;
+    height: 48px;
+    width: 100%;
+    border-radius: 50px;
+    border: none;
+    padding-left: 10px;
+
+    form {
+        display: flex;
+        flex: 1;
+    }
+
+    form  > input {
+        background-color: transparent;
+        border: none;
+        width: 100%;
+        margin-left: 5px;
+        font-size: 16px;
+    }
+
+    form > button {
+        display: none;
+    }
+    input: focus {
+        outline: none;
+    }
+    :hover {
+        background-color: #e1e1e1;
+    }
 `
