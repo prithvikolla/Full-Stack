4f42ee4863fbb54d2bc99d68886ea09f0de25bb8
prithvi <prithvikolla8@gmail.com>
2021-02-25 11:17:39 -0600

    update

--- my-app/src/components/Mainboard.js
+++ my-app/src/components/Mainboard.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-
 function Mainboard() {
     return (
         <div>
             <Wrapper>
                 <Container>
-
+                    <Pin/>
                 </Container>
             </Wrapper>
         </div>
@@ -16,5 +15,15 @@ function Mainboard() {
 export default Mainboard
 
 const Wrapper = styled.div  `
-
+    background-color: white;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    margin-top: 15px;
+    justify-content: center;
+`
+const Container = styled.div  `
+    background-color: white;
+    display: flex;
+    width: 80%;
 `
